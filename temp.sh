701e9bc647bbd349b151570cc2c89c1a1b06b836
be-great <begreat5333@gmail.com>
2025-11-13 01:25:53 +0300

    update

--- temp.sh
+++
@@ -1,7 +0,0 @@
-git filter-branch --force --index-filter \
-  "git rm --cached --ignore-unmatch data/bigdata/appointments.csv \
-   data/bigdata/billing.csv \
-   data/bigdata/patients.csv \
-   data/bigdata/treatments.csv" \
-  --prune-empty --tag-name-filter cat -- --all
-
